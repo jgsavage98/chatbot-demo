3a5442ef1506ba7803b98a8fbefd74e4eea90f97
jgsavage98 <132487760+jgsavage98@users.noreply.github.com>
2023-12-30 17:24:03 -0500

    Update script.js

--- app/static/script.js
+++ app/static/script.js
@@ -4,8 +4,7 @@ function createSession() {
     var goal = document.getElementById('goal').value;
     var duration = document.getElementById('duration').value;
 
-    // Optionally, display a loading indicator here
-
+    // Example fetch call - Replace with your actual API call
     fetch('/create_meditation', {
         method: 'POST',
         headers: {
@@ -14,24 +13,16 @@ function createSession() {
         body: JSON.stringify({ mood, music, goal, duration }),
     })
     .then(response => {
-        // Check if the response is successful
         if (!response.ok) {
             throw new Error('Network response was not ok: ' + response.statusText);
         }
         return response.json();
     })
     .then(data => {
-        console.log('Meditation script:', data.script);
-        // Display the script to the user
         document.getElementById('scriptOutput').textContent = data.script;
-        // Additional processing can be done here if needed
     })
     .catch(error => {
-        console.error('Error:', error);
-        // Update the UI to show the error, for example:
         document.getElementById('errorOutput').textContent = 'An error occurred: ' + error.message;
-    })
-    .finally(() => {
-        // Hide the loading indicator here, if used
     });
 }
+
