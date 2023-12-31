b028f884d179cc76385eddfb5889a63a0664556f
jgsavage98 <132487760+jgsavage98@users.noreply.github.com>
2023-12-31 10:24:53 -0500

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, request, jsonify, render_template
+from flask import Blueprint, request, jsonify, render_template, Response
 import openai  # Assuming you're using the OpenAI Python client
+import azure.cognitiveservices.speech as speechsdk
+import os
 
 main = Blueprint('main', __name__)
 
@@ -25,9 +27,26 @@ def create_meditation():
         )
         script = response.choices[0].text.strip()
 
-        # TODO: Integrate with a text-to-speech service like Descript
+        # Convert the script to speech using Azure Text-to-Speech
+        azure_key = os.getenv('AZURE_TTS_KEY')  # Azure TTS Key
+        azure_region = os.getenv('AZURE_TTS_REGION')  # Azure TTS Region
 
-        return jsonify({"script": script})
+        speech_config = speechsdk.SpeechConfig(subscription=azure_key, region=azure_region)
+        synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
+
+        result = synthesizer.speak_text_async(script).get()
+
+        if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+            # Stream the audio data
+            return Response(result.audio_data, mimetype='audio/wav')
+        else:
+            return jsonify({"error": "Error synthesizing the script"}), 500
+
+
     except Exception as e:
-        return jsonify({"error": str(e)})
+        return jsonify({"error": str(e)}), 500
+        
+#        return jsonify({"script": script})
+#    except Exception as e:
+#        return jsonify({"error": str(e)})
 
